b099889564513e8031a63d054c747825452fc805
xefili <henrycarl2007@gmail.com>
2025-11-17 14:25:53 +0100

    Fix Router Warning

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="app">
     <main>
-      <RouterView />
+      <RouterView v-slot="{Component}">
+        <Transition name="page">
+          <component :is="Component" :key="$route.path" />  
+        </Transition>
+      </RouterView>
     </main>
   </div>
 </template>
+
+<style>
+.page-enter-from {
+  opacity: 0;
+}
+.page-enter-to {
+  opacity: 1;
+}
+
+.page-enter-active {
+  transition: all 100ms ease-in-out;
+}
+</style>
