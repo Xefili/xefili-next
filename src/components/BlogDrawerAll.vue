c92239b75d98224c5dcedfefff9aa429a1103b5a
xefili <henrycarl2007@gmail.com>
2025-11-17 14:26:52 +0100

    Article Archive added

---
+++ src/components/BlogDrawerAll.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { provide } from 'vue';
+import MoreArticles from './MoreArticles.vue';
+
+let raw = await fetch("https://api.xefili.dev/articles");
+var articles = await raw.json().then((data) => {
+    return data.reverse();
+})
+
+let i = 0
+articles.forEach(element => {
+    element.idt = i
+    i++
+});
+
+provide("blog-data", articles);
+</script>
+
+<template>
+    <div v-for="(item, index) in articles" class="dark:text-white w-full">
+        <MoreArticles :article-i-d="item.idt" class="w-full bg-light-el-surface dark:bg-dark-el-surface"/>
+    </div>
+</template>
--- src/views/BlogMore.vue
+++ src/views/BlogMore.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HeaderBar from '@/components/HeaderBar.vue';
+import BlogDrawerAll from '@/components/BlogDrawerAll.vue';
 import StylizedHeading from '@/components/StylizedHeading.vue';
 </script>
 
@@ -7,8 +8,14 @@ import StylizedHeading from '@/components/StylizedHeading.vue';
     <div class="bg-light-background dark:bg-dark-background">
         <HeaderBar />
         <div class="flex flex-col items-center mt-8">
-            <StylizedHeading heading="This page doesn't work yet!" />
-            <p class="dark:text-white lg:w-2/3 bg-red-500/50 dark:bg-red-500/50 p-2 rounded-xl mt-4">As of now, this page does not work yet. If you need to access older articles, visit https://api.xefili.dev/ and fetch them yourself with a tool like Hoppscotch. I will try to fix this page in the near future!</p>
+            <StylizedHeading heading="Article Archive" />
+            <p class="dark:text-white">Articles are sorted from newest to oldest.</p>
+
+            <div class="flex flex-col items-center lg:w-2/5 lg:min-w-2/5 gap-4 mt-4 bg-light-surface dark:bg-dark-surface p-4 m-2 rounded-xl">
+                <Suspense>
+                    <BlogDrawerAll />
+                </Suspense>
+            </div>
         </div>
     </div>
 </template>
