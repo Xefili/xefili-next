b58fd08fa0ac395b57613963ab57789a1d08597c
xefili <henrycarl2007@gmail.com>
2025-11-23 11:56:45 +0100

    Finished Translations

--- src/locales/de.js
+++ src/locales/de.js
@@ -4,6 +4,27 @@ export default {
         subtitle_1: "..für xefili.dev SPA",
         content: "Das Projekt kann mit dem build Befehl auf der package.json Datei kompiliert werden. Der Befehl baut die TailwindCSS Klassen und die Vue SPA App."
     },
+    structure: {
+        title: "Struktur von Xefili.dev",
+        subtitle_1: "Das Gerüst",
+        content_1: "Die App folgt der typischen Vue 3-Struktur. Der Stammordner enthält alle Konfigurationsdateien, der Ordner src/ enthält alle Quelldateien sowie die Eingabedatei main.js. Um alles übersichtlich zu halten, ist der Ordner src/ in weitere Unterordner unterteilt: components/, assets/, locales/, router/, utils/ und views/.",
+        subtitle_2: "Der Ordner views/",
+        content_2: "Der Ordner views enthält alle Seiten und eine grobe Seitenstruktur. Diese Ansichten werden mit Vue Router geroutet und mit Komponenten gefüllt.",
+        subtitle_3: "Der Ordner components/",
+        content_3: "Dieser Ordner enthält die Komponenten, also vorgefertigte HTML-Vorlagen, die in mehreren Ansichten wiederverwendet werden können. Sie sind dynamisch und nutzen Props.",
+        subtitle_4: "Die anderen Ordner",
+        content_4: "Der Ordner „locales/“ enthält die verschiedenen Sprachen für die Dokumentationswebsite. Da ich einen freundlichen Übersetzer an meiner Seite habe, kann ich diese Dokumentation in drei verschiedenen Sprachen anbieten.",
+        content_4_1: "Der Ordner „router/“ enthält das Skript für Vue Router. Er enthält nur Routen und Verweise auf alle Ansichtskomponenten im Ordner views/.",
+        content_4_2: "Der Ordner „utils/“ enthält Setup- und Initialisierungsskripte für Plugins und andere Dinge. Derzeit befindet sich dort nur die Einrichtung für die Shiki-Syntaxhervorhebung.",
+        content_4_3: "Der Ordner assets/ enthält alle Assets wie Schriftarten, Bilder und SVG-Dateien. Der Ordner styles/ enthält das Quell-Stylesheet für TailwindCSS und kann Animationsstile enthalten. Da Stile in Vue normalerweise in ihrem Umfang begrenzt sind, sollten Sie nicht mit vielen Dateien in diesem Ordner rechnen."
+    },
+    contribute: {
+        title: "Wie Sie zu Xefili.dev beitragen können",
+        subtitle_1: "Auf Github",
+        content: "Sie können auf Github beitragen, indem Sie Issues öffnen oder Pull-Anfragen erstellen. Weitere Informationen finden Sie unter github.com/xefili/xefili-app.",
+        subtitle_2: "Auf dieser Seite",
+        content_2: "Wenn auf dieser Seite ein Fehler auftritt, klicken Sie auf das Symbol in der Kopfzeile, um die Seite zu melden (derzeit nicht implementiert!)."
+    },
     navigation: {
         home: "Hauptseite",
         report: "Fehler melden"
--- src/locales/fr.js
+++ src/locales/fr.js
@@ -11,12 +11,20 @@ export default {
         subtitle_2: "Le views/ fichier",
         content_2: " Le fichier perspectives contient toutes les pages et une une structure de page approximative. Ces vues sont acheminées avec le routeur perspectives et remplies de composants.",
         subtitle_3: "Le components/ fichier",
-        content_3: "",
-        subtitle_4: "",
-        content_4: "",
-        content_4_1: "",
-        content_4_2: "",
-        content_4_3: ""
+        content_3 : "Ce dossier contient les composants ; des modèles HTML pré-construits réutilisables dans plusieurs vues. Ils sont dynamiques et utilisent des props.",
+        subtitle_4 : "Les autres dossiers",
+        content_4 : "Le dossier locales/ contient les différentes langues pour le site web de documentation. Grâce à mon aimable traducteur, je suis en mesure de fournir cette documentation en 3 langues différentes.",
+        content_4_1 : "Le dossier router/ contient le script pour Vue Router. Il ne contient que des routes et des références à tous les composants d'affichage dans le dossier views/",
+        content_4_2 : "Le dossier utils/ contient les scripts de configuration et d'initialisation pour les plugins et autres éléments. Actuellement, seule la configuration pour la coloration syntaxique Shiki s'y trouve.",
+        content_4_3: "Le dossier assets/ contient tous les actifs tels que : polices, images et fichiers svg. Le dossier styles/ contient la feuille de style source pour TailwindCSS et peut contenir des styles d'animation. Étant donné que les styles sont généralement limités à Vue, vous ne devriez pas vous attendre à y trouver beaucoup de fichiers."
+
+    },
+    contribute: {
+        title: "Comment contribuer à Xefili.dev",
+        subtitle_1: "Sur Github",
+        content: " Vous pouvez contribuer sur Github en ouvrant des tickets ou en créant des pull requests. Rendez-vous sur github.com/xefili/xefili-app pour en savoir plus.",
+        subtitle_2: "Sur cette page",
+        content_2: " Si cette page contient une erreur, cliquez sur l'icône dans l'en-tête pour la signaler (fonctionnalité non implémentée pour le moment !)"
     },
     navigation: {
         home: "Accueil",
