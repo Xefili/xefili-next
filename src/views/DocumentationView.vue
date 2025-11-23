cffea8aabb2102671d932c681633533c8df48a46
xefili <henrycarl2007@gmail.com>
2025-11-23 12:07:00 +0100

    Card to new pages

--- src/views/DocumentationView.vue
+++ src/views/DocumentationView.vue
@@ -8,12 +8,20 @@ import StylizedHeading from '@/components/StylizedHeading.vue';
     <div class="bg-light-background dark:bg-dark-background">
         <HeaderBar />
 
-        <StylizedHeading heading="Documentation" />
+        <div class="flex w-full flex-col items-center">
+            <StylizedHeading class="my-4" heading="Documentation" />
 
-        <div>
-            <DefaultCard title="Structure & Build" fab-icon="build">Learn how to navigate and build xefili.dev from source.</DefaultCard>
-            <DefaultCard title="Contribute" fab-icon="crowdsource">Contribute to the project on Github, fix bugs and get your name displayed in the HELPERS.md file.</DefaultCard>
+            <div class="flex flex-row flex-wrap lg:w-2/3 justify-evenly gap-4">
+                <RouterLink to="/documentation/build">
+                    <DefaultCard title="Build" fab-icon="build">Learn how to navigate and build xefili.dev from source.</DefaultCard>
+                </RouterLink>
+                <RouterLink to="/documentation/structure">
+                    <DefaultCard title="Structure" fab-icon="folder_code">See how to project is structured and navigate more easily around the source code.</DefaultCard>
+                </RouterLink>
+                <RouterLink to="/documentation/contribute">
+                    <DefaultCard title="Contribute" fab-icon="crowdsource">Contribute to the project on Github, fix bugs and get your name displayed in the HELPERS.md file.</DefaultCard>
+                </RouterLink>
+            </div>
         </div>
-        <RouterLink to="/documentation/build" class="text-white">click mee</RouterLink>
     </div>
 </template>
